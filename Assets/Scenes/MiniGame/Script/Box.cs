e38addc2ddeb836523fbf9aa4225581cf0b3a04e
NongHaru <6306021422194@fitm.kmutnb.ac.th>
2021-10-11 16:38:25 +0700

    test

---
+++ Assets/Scenes/MiniGame/Script/Box.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Box : MonoBehaviour
+{
+    private float min_x = -2.32f, max_x = 2.32f;
+    private bool canMove;
+    private float move_speed = 50f;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+    }
+    void Start()
+    {
+        canMove = true;
+        if (Random.Range(0, 2) > 0)
+        {
+            move_speed *= 5f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBox();
+        if (Input.GetMouseButtonDown(0))
+        {
+            DropBox();
+        }
+    }
+
+    void MoveBox()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x += move_speed * Time.deltaTime;
+            if (temp.x>max_x)
+            {
+                move_speed *= -1f;
+            }else if (temp.x < min_x)
+            {
+                move_speed *= -1f;
+            }
+            transform.position = temp;
+        }
+    }
+
+    public void DropBox()
+    {
+        canMove = false;
+        rb.gravityScale = Random.Range(2,4);
+    }
+}
