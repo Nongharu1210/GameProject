dc457e7565e011f416f4c79728693188ae288b47
NongHaru <6306021422194@fitm.kmutnb.ac.th>
2021-10-17 13:36:02 +0700

    car

--- Assets/Script/UI/Menu/MenuController.cs
+++ Assets/Script/UI/Menu/MenuController.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
-    [SerializeField] 
-    private GameObject noSevedGameDialogue = null;
+    [SerializeField] private GameObject noSevedGameDialogue = null;
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_TextElement volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject ConfirmationPrompt = null;
     
     public void NewGameDialogueYes()
     {
@@ -28,6 +34,25 @@ public class MenuController : MonoBehaviour
         {
             noSevedGameDialogue.SetActive(true);
         }
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        //volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        ConfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        ConfirmationPrompt.SetActive(false);
     }
 
     public void ExitBtn()
